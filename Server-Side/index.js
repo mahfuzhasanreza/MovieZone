4336c98b57401b288531f45d5cdea2920aea2381
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-12-22 22:47:55 +0600

    added index.js

---
+++ Server-Side/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdfgd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('movie server is running');
+})
+
+app.listen(port, () => {
+    console.log(`movie server is running at PORT: ${port}`);
+})
